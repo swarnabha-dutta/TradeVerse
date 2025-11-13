7cf6c89ec68e04824a448f80217b1240aa9c614a
swarnabha <swarnabhadutta909@gmail.com>
2025-11-13 06:27:55 +0530

    Implement User DashBoard and setup the frontend part(if necessary)

---
+++ .history/dashboard/src/components/Home_20251112150317.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Dashboard from "./DashBoard";
+import TopBar from "./TopBar";
+
+const Home = () => {
+    return (
+        <>
+        <TopBar />
+        <Dashboard />
+        </>
+    );
+};
+
+export default Home;
---
+++ dashboard/src/components/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Dashboard from "./DashBoard.jsx";
+import TopBar from "./TopBar";
+
+const Home = () => {
+    return (
+        <>
+        <TopBar />
+        <Dashboard />
+        </>
+    );
+};
+
+export default Home;
